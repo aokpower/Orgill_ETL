e58600b6639c12feb521cab92b6293cae47bd379
Cooper LeBrun <cooperlebrun@gmail.com>
2016-08-30 14:32:33 -0700

    fix LegSource.parse specs being in wrong scope

--- spec/lib/products/leg_extractor_spec.rb
+++ spec/lib/products/leg_extractor_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe LegSource do
           # I don't want to worry about file reading logic when I'm
           # just trying to test internal plumbing.
           actual = LegSource.new(data: 'foo').products
-          # TODO: replace with something better than a nil check! after other tests are hammered out.
+          # TODO: replace with something better than a nil check!
           expect(actual).to_not be_nil
         end
 
@@ -48,41 +48,44 @@ RSpec.describe LegSource do
         it 'both data and file' do
           use_tmp_file do |file|
             expect { LegSource.new(data: 'foo', file: file.path) }
-              .to raise_error(ArgumentError)
+            .to raise_error(ArgumentError)
           end
-        end
-      end
-
-      context 'file:' do
-        it 'reads file into @source as a string' do
-          content = 'foo'
-          use_tmp_file(content) do |file|
-            actual = LegSource.new(file: file).source
-            expect(actual).to eq content
-          end
-        end
-      end
-
-      context 'parsing' do
-        it 'detabularizes with no map' do
-          actual   = LegSource.parse(short_data)
-          expected = [['foo', 'bar', 'baz'], ['boo', '', 'biz']]
-          expect(actual).to eq expected
-        end
-
-        it 'can take an index map' do
-          test_map = { 0 => 'one', 1 => 'two', 2 => 'three' }
-          expected = [
-            { 'one' => 'foo', 'two' => 'bar', 'three' => 'baz'},
-            { 'one' => 'boo', 'two' => '', 'three' => 'biz'}
-          ]
-          actual = LegSource.parse(short_data, index_map: test_map)
-          expect(actual).to eq expected
         end
       end
     end
 
     context 'each' do
+      it 'yields products' do
+      end
+    end
+
+    context 'file:' do
+      it 'reads file into @source as a string' do
+        content = 'foo'
+        use_tmp_file(content) do |file|
+          actual = LegSource.new(file: file).source
+          expect(actual).to eq content
+        end
+      end
+    end
+
+  end
+
+  context 'parsing' do
+    it 'detabularizes with no map' do
+      actual   = LegSource.parse(short_data)
+      expected = [['foo', 'bar', 'baz'], ['boo', '', 'biz']]
+      expect(actual).to eq expected
+    end
+
+    it 'can take an index map' do
+      test_map = { 0 => 'one', 1 => 'two', 2 => 'three' }
+      expected = [
+        { 'one' => 'foo', 'two' => 'bar', 'three' => 'baz'},
+        { 'one' => 'boo', 'two' => '', 'three' => 'biz'}
+      ]
+      actual = LegSource.parse(short_data, index_map: test_map)
+      expect(actual).to eq expected
     end
   end
 
