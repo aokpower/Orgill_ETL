88a4dc45088c566e895e9fb027a846e46a32a8a1
Cooper LeBrun <cooperlebrun@gmail.com>
2016-08-31 09:07:24 -0700

    add row_separator optional argument, documentation to #detabularize

--- lib/orgill/products/leg_extractor.rb
+++ lib/orgill/products/leg_extractor.rb
@@ -13,6 +13,7 @@ module Orgill
       # @example
       #   Orgill::Products::LegSource.parse("foo~   bar ~ baz ~\r\nboo~ ~biz")
       #     #=> [['foo', 'bar', 'baz'], ['boo', '', 'biz']]
+      # @see detabularize
       def self.parse(string, index_map: nil)
         detabularize(string).map do |row|
           unless index_map.nil?
@@ -44,9 +45,12 @@ module Orgill
 
         private
 
-        def detabularize(string)
+        # @param row_seperator [String] "\r\n" by default.
+        #   Try changing this to "\n" if you are having problems
+        def detabularize(string, row_separator = "\r\n")
+          # Orgill uses a tilda ('~') delimited 'spreadsheet' sort of file.
           string
-            .split("\r\n") # split products
+            .split(row_separator) # split products
             .map { |row| row.split(/\s*~\s*/) } # split product fields
         end
       end
