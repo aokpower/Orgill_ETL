9afc867b9031e09a9a8a225de7bb5c76e04d4a69
Cooper LeBrun <cooperlebrun@gmail.com>
2016-09-02 13:36:36 -0700

    add stub image_transform

---
+++ spec/lib/image_transform_spec.rb
@@ -0,0 +1,18 @@
+require_relative '../../lib/image_transform.rb'
+
+RSpec.describe ImageTransform do
+  # context 'get image file' do
+  #   it 'gets ftp location'
+  #   it 'downloads file'
+  # end
+
+  # context 'upload image, get public link' do
+  #   it 'uploads to s3'
+  #   it 'makes file pubic'
+  #   it 'retrievs url'
+  # end
+
+  # context 'insert product' do
+  #   it 'inserts public url into product'
+  # end
+end
