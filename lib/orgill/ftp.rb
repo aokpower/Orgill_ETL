d35fc4b1300af6454cc2bb023cee771afa665dbc
Cooper LeBrun <cooperlebrun@gmail.com>
2016-09-07 16:23:00 -0700

    add FTPImageHelper#pick_latest_folder. Needs refactor

--- lib/orgill/ftp.rb
+++ lib/orgill/ftp.rb
@@ -1,6 +1,33 @@
+require 'dotenv'
+Dotenv.load
+
 module Orgill
-  module FTP
-    BaseURL  = 'ftp.orgill.com'
-    BasePath = '/orgillftp/webfiles'
+  class FTP
+    #extend Orgill::Products::FTPImageHelper # why unintialized constant error?
+
+    Base_URL  = 'ftp.orgill.com'.freeze
+    Base_Path = '/orgillftp/webfiles'.freeze
+
+    def initialize
+      @credentials = {
+        username: ENV['ORGILL_FTP_USERNAME'],
+        password: ENV['ORGILL_FTP_PASSWORD']
+      }
+    end
+  end
+end
+
+module Orgill
+  module Products
+    class FTPImageHelper
+      class << self
+        def pick_latest_folder(folder_list)
+          # NEED REFACTOR BADLY
+          folder_list.select { |f_name| f_name[/week\d+/i] }
+          .sort_by { |f_name| /week(\d+)/i.match(f_name).captures[0] }
+          .last # because it sorts lowest -> highest
+        end
+      end
+    end
   end
 end
--- spec/lib/orgill/ftp_spec.rb
+++ spec/lib/orgill/ftp_spec.rb
@@ -2,12 +2,39 @@ require_relative '../../../lib/orgill/ftp'
 
 RSpec.describe Orgill::FTP do
   context 'address assembley' do
+    # Sanity tests
     it 'has the right base address' do
-      expect(Orgill::FTP::BaseURL).to eq 'ftp.orgill.com'
+      expect(Orgill::FTP::Base_URL).to eq 'ftp.orgill.com'
     end
 
     it 'has base path' do
-      expect(Orgill::FTP::BasePath).to eq '/orgillftp/webfiles'
+      expect(Orgill::FTP::Base_Path).to eq '/orgillftp/webfiles'
     end
   end
+end
+
+FTPImageHelper = Orgill::Products::FTPImageHelper
+
+RSpec.describe FTPImageHelper do
+  it 'can pick most recent image folder from list' do
+    # orgill has images organized like so:
+    # images/week29/*.JPG
+    # this the "week*" folder is changed periodically, so We will need to
+    # request directory contents and match the latest folder, either through
+    # a regex, or through folder modification dates.
+
+    folder_list = %w(
+      Foox.xzf
+      Week28
+      Week29
+      Week32
+      Week30
+      Week31
+    )
+
+    actual = FTPImageHelper.pick_latest_folder(folder_list)
+    expect(actual).to eq 'Week32'
+  end
+
+  # TODO: add VCR integration_test for this.
 end
