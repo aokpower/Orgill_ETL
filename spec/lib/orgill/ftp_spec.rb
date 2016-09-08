0466e64dcccd4535cf6da519ea62b8270b47add5
Cooper LeBrun <cooperlebrun@gmail.com>
2016-09-08 09:57:31 -0700

    Add context, return nil when no good input spec.

--- spec/lib/orgill/ftp_spec.rb
+++ spec/lib/orgill/ftp_spec.rb
@@ -16,24 +16,35 @@ end
 FTPImageHelper = Orgill::Products::FTPImageHelper
 
 RSpec.describe FTPImageHelper do
-  it 'can pick most recent image folder from list' do
-    # orgill has images organized like so:
-    # images/week29/*.JPG
-    # this the "week*" folder is changed periodically, so We will need to
-    # request directory contents and match the latest folder, either through
-    # a regex, or through folder modification dates.
+  context '#pick_latest_folder' do
+    it 'can pick most recent image folder from list' do
+      # orgill has images organized like so:
+      # images/week29/*.JPG
+      # this the "week*" folder is changed periodically, so We will need to
+      # request directory contents and match the latest folder, either through
+      # a regex, or through folder modification dates.
 
-    folder_list = %w(
-      Foox.xzf
-      Week28
-      Week29
-      Week32
-      Week30
-      Week31
-    )
+      folder_list = %w(
+        Foox.xzf
+        Week28
+        Week29
+        Week32
+        Week30
+        Week31
+      )
 
-    actual = FTPImageHelper.pick_latest_folder(folder_list)
-    expect(actual).to eq 'Week32'
+      actual = FTPImageHelper.pick_latest_folder(folder_list)
+      expect(actual).to eq 'Week32'
+    end
+
+    it 'returns nil if no appropriate folder' do
+      folder_list = ['Foo.xzf']
+
+      actual = FTPImageHelper.pick_latest_folder(folder_list)
+      expect(actual).to be_nil
+      actual_empty = FTPImageHelper.pick_latest_folder([])
+      expect(actual_empty).to be_nil
+    end
   end
 
   # TODO: add VCR integration_test for this.
